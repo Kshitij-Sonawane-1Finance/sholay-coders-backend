2102e4bae8274c2911da59d6e07156f7448e906b
Suraj Mishra <suraj.mishra@1finance.org.in>
2025-01-11 05:30:57 +0530

    CR: API Added

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def extract_text_from_pdf(file_path):
                 text += page.extract_text()
     return text
 
-def extract_question_from_file(file_path, file_extension):
+def extract_question_from_file(name, url, file_path, file_extension):
     if file_extension == '.csv':
         df = pd.read_csv(file_path) 
     else:
@@ -68,7 +68,7 @@ def extract_question_from_file(file_path, file_extension):
     return questions_answers
 
             
-def generate_questions(text, count):
+def generate_questions(name, url, text, count):
     try :
         template = """
         You are Question Answer Generation Expert who is expert in question generation and is proficient in creating detailed and well-explained answers.
@@ -89,27 +89,44 @@ def generate_questions(text, count):
          raise HTTPException(status_code=500, detail=f"Error generating questions: {e}")
     
 @app.post("/upload_file",)
-async def upload_file(file: UploadFile = File(...)):
+async def upload_file(name: str,url:str, dynamic: bool, count:str, file: UploadFile = File(...)):
     if not file:
         raise HTTPException(status_code=400, detail="No file uploaded")
     else:
-        destination = f"uploads/{file.filename}"
+        folder_path = "uploads"
+
+        if os.listdir(folder_path):
+            for file_name in os.listdir(folder_path):
+                file_path = os.path.join(folder_path, file_name)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+
+        destination = f"{folder_path}/{file.filename}"
         with open(destination, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
         file_extension = os.path.splitext(destination)[1].lower()
-        if file_extension == '.pdf':
+        
+        if file_extension == '.pdf' and dynamic:
             pdfText = extract_text_from_pdf(destination)
             if pdfText:
-                pdfText = generate_questions(pdfText, 5)
+                pdfText = generate_questions(name, url, pdfText, count)
                 return pdfText
             else:
                 raise HTTPException(status_code=400, detail="PDF text extraction failed.")
-        elif file_extension in ['.xls', '.xlsx', '.csv']: 
-            pdfText = extract_question_from_file(destination, file_extension)
+        elif file_extension in ['.xls', '.xlsx', '.csv'] and not dynamic: 
+            pdfText = extract_question_from_file(name, url, destination, file_extension)
             return pdfText
         else:
             raise HTTPException(status_code=400, detail="Unsupported file format.")
+        
+@app.post("/analyze_model",)
+async def analyze_model():
+    return {"message": "Welcome to Sholay Coders!"}
 
+@app.post("/model_overview",)
+async def model_overview():
+    return {"message": "Welcome to Sholay Coders!"}
 
-
-
+@app.post("/dashboard",)
+async def dashboard():
+    return {"message": "Welcome to Sholay Coders!"}
